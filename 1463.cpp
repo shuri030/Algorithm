9d54ebcc6cd0edb996b3667dcd15ea8a6a10deb3
shuri030 <kpo01286@gmail.com>
2018-08-01 00:28:40 +0900

    BOJ 1463: 1로 만들기
    BOJ 1463: 1로 만들기

---
+++ 1463.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <limits.h>
+#define MAX 1000010
+using namespace std;
+
+int check[MAX];
+int n;
+void cnt(int n) 
+{
+	if(n < 1) {
+		return;
+	}
+	if(n % 3 == 0) {
+		if(check[n / 3] > check[n] + 1) {
+			check[n / 3] = check[n] + 1;
+			cnt(n / 3);
+		}
+	}
+	if(n % 2 == 0) {
+		if(check[n / 2] > check[n] + 1) {
+			check[n / 2] = check[n] + 1;
+			cnt(n / 2);
+		}
+	}
+	if(check[n - 1] > check[n] + 1) {
+		check[n - 1] = check[n] + 1;
+		cnt(n - 1);
+	}
+
+
+}
+void init() 
+{
+	for(int i = 0; i < MAX; i++) {
+		check[i] = INT_MAX / 2;
+	}
+}
+
+int main()
+{
+	scanf("%d", &n);
+
+	init();
+
+	check[n] = 0;
+	cnt(n);
+
+	printf("%d", check[1]);
+}
