734b7c9dd47c1967ca012f21e187e613804c2a11
shuri030 <kpo01286@gmail.com>
2018-08-01 02:20:48 +0900

    BOJ 3052: 나머지
    BOJ 3052: 나머지

---
+++ 3052.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+
+int remain[43];
+
+int main(void)
+{
+	int num, cnt = 0;
+
+	for(int i = 0; i < 10; i++) {
+		scanf("%d", &num);
+		if(remain[num % 42] == 0) {
+			remain[num % 42] = 1;
+			cnt++;
+		}
+	}
+	printf("%d", cnt);
+
+	return 0;
+}
