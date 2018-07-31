0e24315bcfffbd551c628301565debafc1958b59
shuri030 <kpo01286@gmail.com>
2018-08-01 00:20:50 +0900

    BOJ 1182: 부분집합의 합
    BOJ 1182: 부분집합의 합

---
+++ 1182.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+
+int N, S;
+int num[25];
+int cnt = 0;
+
+
+void cal(int n, int sum)
+{
+	if(n > N) {
+		return;
+	}
+	
+	//포함
+	sum = sum + num[n];
+	if(sum == S) {
+		cnt++;
+	}
+	cal(n + 1, sum);
+
+	sum = sum - num[n];
+	cal(n + 1, sum);
+
+	return;
+}
+
+int main(void)
+{
+	scanf("%d %d", &N, &S);
+
+	for(int i = 1; i <= N; i++) {
+		scanf("%d", &num[i]);
+	}
+
+	cal(1, 0);
+	
+	printf("%d", cnt);
+	return 0;
+}
