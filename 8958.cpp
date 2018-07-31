f14bc8b906fda0c98aec124656374e5df6aaec4d
shuri030 <kpo01286@gmail.com>
2018-08-01 02:10:40 +0900

    BOJ 8958: OX퀴즈
    BOJ 8958: OX퀴즈

---
+++ 8958.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+
+char res[83];
+
+int main(void)
+{
+	int tc, point, st;
+	scanf("%d", &tc);
+
+	for(int t = 1; t <= tc; t++) {
+		point = 0; st = 1;
+		memset(res, 0, sizeof(res));
+		scanf("%s", res);
+
+		for(int i = 0; i < strlen(res); i++) {
+			if(res[i] == 'O') {
+				point += st;
+				st++;
+			}
+			else if(res[i] == 'X') {
+				st = 1;
+			}
+		}
+		printf("%d\n", point);
+	}
+
+	return 0;
+}
