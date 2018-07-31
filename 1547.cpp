0f1d6373a8733b863b6f2f410daec21cd131c403
shuri030 <kpo01286@gmail.com>
2018-08-01 00:31:00 +0900

    BOJ 1547: 공
    BOJ 1547: 공

---
+++ 1547.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int M, a, b, ball = 1;
+	scanf("%d", &M);
+
+	for(int i = 1; i <= M; i++) {
+		scanf("%d %d", &a, &b);
+
+		if(a == ball) {
+			ball = b;
+		}
+		else if(b == ball) {
+			ball = a;
+		}
+	}
+
+	printf("%d", ball);
+	return 0;
+}
