1c3734d7c14a4cda48c96fb7cf4e7bb87aea9629
shuri030 <kpo01286@gmail.com>
2018-07-31 23:39:10 +0900

    BOJ 1032: 명령 프롬프트
    BOJ 1032: 명령 프롬프트

---
+++ 1032.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <limits.h>
+
+using namespace std;
+
+char word[55][55];
+char ans[55];
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    
+    for(int i = 0; i < N; i++) {
+        scanf("%s", word[i]);
+    }
+    
+    for(int i = 0; i < strlen(word[0]); i++) {
+        ans[i] = word[0][i];
+    }
+
+    
+    for(int i = 0; i < strlen(word[0]); i++) {
+        for(int j = 0; j < N - 1; j++) {
+            if(word[j][i] != word[j + 1][i]) {
+                ans[i] = '?';
+                break;
+            }
+            else {
+                ans[i] = word[j][i];
+            }
+        }
+    }
+
+    
+    for(int i = 0; i < strlen(word[0]); i++) {
+        printf("%c", ans[i]);
+    }
+    return 0;
+}
