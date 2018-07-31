781ff95d15fb69e1b0b8090d8d17d9e53e75263b
shuri030 <kpo01286@gmail.com>
2018-08-01 00:25:18 +0900

    BOJ 1316: 그룹 단어 체커
    BOJ 1316: 그룹 단어 체커

---
+++ 1316.cpp
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+using namespace std;
+
+int alpha[30];
+
+
+int main() 
+{
+	int N;
+	int cnt = 0;
+	scanf("%d", &N);
+
+	for(int i = 1; i <= N; i++) {
+		memset(alpha, 0, sizeof(alpha));
+		char word[105];
+		scanf("%s", word);
+
+		int j = 0, flag = 0;
+		while(j <= (strlen(word) - 1)) {
+			if(word[j] == word[j + 1]) {
+				j++;
+			}
+			else if(word[j] != word[j + 1]) {
+				if(alpha[word[j + 1] - 'a'] == 1) {
+					flag = -1;
+					break;
+				}
+				else {
+					alpha[word[j] - 'a'] = 1;
+					j++;
+				}
+			}
+		}
+
+		if(flag != -1) {
+			cnt++;
+		}
+	}
+
+	printf("%d", cnt);
+	
+
+    return 0;
+}
