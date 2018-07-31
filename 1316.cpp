66b4800b8fb3e4d17022c8c42c2cae26d637d108
Cho Hanseul <41959385+shuri030@users.noreply.github.com>
2018-08-01 00:42:24 +0900

    BOJ 1316: 그룹 단어 체커

--- 1316.cpp
+++ 1316.cpp
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <iostream>
 #include <string.h>
 #include <vector>
@@ -20,22 +20,22 @@ int main()
 
 		int j = 0, flag = 0;
 		while(j <= (strlen(word) - 1)) {
-			if(word[j] == word[j + 1]) {
+			if(word[j] == word[j + 1]) { //같은문자면 인덱스 증가
 				j++;
 			}
-			else if(word[j] != word[j + 1]) {
-				if(alpha[word[j + 1] - 'a'] == 1) {
+			else if(word[j] != word[j + 1]) { //다음꺼랑 비교해서 다르면
+				if(alpha[word[j + 1] - 'a'] == 1) { // 다음꺼가 이미 방문했으면
 					flag = -1;
 					break;
 				}
-				else {
+				else { //아니면 방문체크
 					alpha[word[j] - 'a'] = 1;
 					j++;
 				}
 			}
 		}
 
-		if(flag != -1) {
+		if(flag != -1) { //그룹아니면 증가
 			cnt++;
 		}
 	}
