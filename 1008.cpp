6386b3d0b57740123936a666f72e39781dac7954
shuri030 <kpo01286@gmail.com>
2018-07-31 23:15:16 +0900

    BOJ 1008: A/B
    백준 1008번 코드

---
+++ 1008.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+
+	cout.setf(ios::fixed);
+	cout.precision(9); //소수점 9자리까지
+
+    cout << a/b;
+}
