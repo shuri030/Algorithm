3dfe00485de21f568a805a1d0cdf07419bbf72c5
shuri030 <kpo01286@gmail.com>
2018-08-01 00:28:08 +0900

    BOJ 1449: 수리공 항승
    BOJ 1449: 수리공 항승

---
+++ 1449.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int num[1005];
+int N, L;
+
+int main(void)
+{
+	int cnt = 0, tmp;
+
+	scanf("%d %d", &N, &L);
+	for(int i = 0; i < N; i++) {
+		scanf("%d", &num[i]);
+	}
+	sort(num, num + N);
+	tmp = 0;
+
+	for(int i = 0; i < N; i++) {
+		if(num[i] - num[tmp] <= L - 1) {
+			continue;
+		}
+		cnt++;
+		tmp = i;
+	}
+	printf("%d", cnt + 1);
+	return 0;
+}
