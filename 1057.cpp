eb6ca181671ae71f04425b1f6493153380a531ce
shuri030 <kpo01286@gmail.com>
2018-07-31 23:41:32 +0900

    BOJ 1057: 토너먼트
    BOJ 1057: 토너먼트

---
+++ 1057.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int N, a, b;
+    scanf("%d %d %d", &N, &a, &b);
+    
+    int play = ceil(log2(N)); //사람이 N명일때, 총 경기수
+    int answer = 0;
+    
+    for(int i = 1; i <= play; i++) {
+        
+        int round = pow(2, i);
+        if ((a - 1)/round == (b - 1)/round) { // 같이 경기하면
+            answer = i;
+            break;
+        }
+    }
+    
+    printf("%d", answer);
+    
+    return 0;
+}
