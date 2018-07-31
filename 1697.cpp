41bb5b136c6959386a100c4761d86afda8c94f07
shuri030 <kpo01286@gmail.com>
2018-08-01 00:32:31 +0900

    BOJ 1697: 숨바꼭질
    BOJ 1697: 숨바꼭질

---
+++ 1697.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <limits.h>
+
+using namespace std;
+
+int n, k;
+int visit[1000010];
+int ans = 0;
+
+int BFS(int n) {
+    
+    visit[n] = 0;
+    queue<int> q;
+    int cur;
+    cur = n;
+    q.push(cur);
+    
+    while(!q.empty()) {
+        cur = q.front();
+        q.pop();
+        
+        if((cur + 1) <= 100000 && visit[cur + 1] > visit[cur] + 1) {
+            q.push(cur + 1);
+            visit[cur + 1] = visit[cur] + 1;
+        }
+        
+        if((cur - 1) >= 0 && (cur - 1) <= 100000 && visit[cur - 1] > visit[cur] + 1) {
+            q.push(cur - 1);
+            visit[cur - 1] = visit[cur] + 1;
+        }
+        
+        if((cur * 2) <= 100000 && visit[cur * 2] > visit[cur] + 1) {
+            q.push(cur * 2);
+            visit[cur * 2] = visit[cur] + 1;
+        }
+        
+    }
+    
+    return visit[k];
+    
+}
+int main()
+{
+
+    scanf("%d %d", &n, &k);
+    
+    for(int i = 0; i <= 100000; i++) {
+        visit[i] = INT_MAX / 2;
+    }
+    
+    printf("%d", BFS(n));
+  
+    return 0;  
+}
