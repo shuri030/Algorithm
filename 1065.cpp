abfdb9dcf86d107c86b899780df058714cdf1196
shuri030 <kpo01286@gmail.com>
2018-07-31 23:43:28 +0900

    BOJ 1065: 한수
    BOJ 1065: 한수

---
+++ 1065.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int cnt = 0;
+	int a, b, c;
+
+	cin >> n;
+	
+	for(int i = 1; i <= n; i++) {
+
+		if(i >= 1 && i <= 99) {
+			cnt++;
+		}
+		else if(i == 1000) {
+			break;
+		}
+		else {
+			a = i % 1000 / 100;
+			b = i % 100 / 10;
+			c = i % 10;
+
+			if(a - b == b - c)
+				cnt++;
+		}
+		
+	}
+	printf("%d", cnt);
+	return 0;
+}
