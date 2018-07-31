76ec10774300b699e95f9cd34f44df092b38e35d
shuri030 <kpo01286@gmail.com>
2018-08-01 01:52:42 +0900

    BOJ 2752: 세수정렬
    BOJ 2752: 세수정렬

---
+++ 2752.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int num[3];
+
+int main(void)
+{
+	for(int i = 0; i < 3; i++) {
+		scanf("%d", &num[i]);
+	}
+	sort(num, num + 3);
+
+	for(int i = 0; i < 3; i++) {
+		printf("%d ", num[i]);
+	}
+	return 0;
+}
