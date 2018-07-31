fe492ec3d77017ef0d881812fb58b9a7accc1ad3
shuri030 <kpo01286@gmail.com>
2018-08-01 00:19:14 +0900

    BOJ 1152: 단어의 개수
    BOJ 1152: 단어의 개수

---
+++ 1152.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+#define SEN_MAX 1000003
+
+using namespace std;
+
+int main() {
+
+	char sen[SEN_MAX];
+	int cnt = 0;
+	bool check = false;
+	int i = 0;
+	while(1) {
+
+		scanf("%c", &sen[i]); 
+		if(sen[i] == '\n' || sen[i] == '\0') {
+			break;
+		}
+		else if(sen[i] == ' ' && check == true) {
+			check = false;
+		}
+		else if(sen[i] != ' ') {
+			if(check == false) {
+				cnt++;
+			}
+			check = true;
+		}
+		
+		i++;
+
+	}
+	//printf("%d\n", strlen(sen));
+	printf("%d\n", cnt);
+
+}
