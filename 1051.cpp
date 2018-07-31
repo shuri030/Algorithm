589d3ba3106ea6e4d94f4824afbe6a066f70e752
shuri030 <kpo01286@gmail.com>
2018-07-31 23:38:52 +0900

    BOJ 1051: 숫자 정사각형
    BOJ 1051: 숫자 정사각형

---
+++ 1051.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int board[55][55];
+
+int main(void)
+{
+	int m = 1, k;
+	scanf("%d %d", &N, &M);
+	k = min(N, M);
+
+	for(int i = 1; i <= N; i++) {
+		for(int j = 1; j <= M; j++) {
+			scanf("%1d", &board[i][j]);
+		}
+	}
+	for(int i = 1; i <= N - 1; i++) {
+		for(int j = 1; j <= M - 1; j++) {
+			for(int x = 1; x <= k; x++) {
+				if(((i + x) <= N) && ((j + x) <= M)) {
+					if((board[i][j] == board[i + x][j]) && (board[i + x][j] == board[i][j + x]) && (board[i][j + x] == board[i + x][j + x])) {
+						m = max(m, (x + 1)*(x + 1));
+					}
+				}
+			}
+		}
+	}
+
+	printf("%d\n", m);
+
+	return 0;
+}
