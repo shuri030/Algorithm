a575315da1ab0cc7b23514d32e48ae6ec2129961
shuri030 <kpo01286@gmail.com>
2018-08-01 00:34:42 +0900

    BOJ 1748: 수 이어 쓰기1
    BOJ 1748: 수 이어 쓰기1

---
+++ 1748.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int sum = 0;
+	
+	scanf("%d", &n);
+
+	for(int i = 1; i <= n; i*=10) {
+		sum += (n - i + 1);
+	}
+
+	printf("%d", sum);
+  
+   return 0;
+}
