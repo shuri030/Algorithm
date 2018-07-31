e88f4545fbfbb0c45f80dc318ff6347f2cb0b718
shuri030 <kpo01286@gmail.com>
2018-08-01 00:18:10 +0900

    BOJ 1149: RGB거리
    BOJ 1149: RGB거리

---
+++ 1149.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int cost[1010][3];
+int dp[1010][3];
+
+int main()
+{
+//첫 코드
+	int r, g, b, n; //0 = r, 1 = g, 2 = b;
+
+	scanf("%d", &n);
+
+	for(int i = 1; i <= n; i++) {
+		scanf("%d %d %d", &r, &g, &b);
+		cost[i][0] = r;
+		cost[i][1] = g;
+		cost[i][2] = b;
+	}
+	
+	dp[1][0] = cost[1][0];
+	dp[1][1] = cost[1][1];
+	dp[1][2] = cost[1][2];
+	
+	for(int i = 2; i <= n; i++) {
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+	}
+	
+	int ans = min(dp[n][0], dp[n][1]);
+	ans = min(ans, dp[n][2]);
+
+	printf("%d", ans);
+}
---
+++ 1149_2.cpp
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <limits.h>
+#include <vector>
+#include <queue>
+
+using namespace std;
+int n;
+int board[1005][4];
+int dp[1005][4];
+
+int cal(int h, int c)
+{
+    if(h > n) {
+        return 0;
+    }
+    
+    int &ret = dp[h][c];
+    
+    if(ret != -1) {
+        return ret;
+    }
+    
+    ret = INT_MAX / 2;
+    
+    if(c == 1) {
+        ret = min(ret, cal(h + 1, 2) + board[h][1]);
+        ret = min(ret, cal(h + 1, 3) + board[h][1]);
+    }
+    else if(c == 2) {
+        ret = min(ret, cal(h + 1, 1) + board[h][2]);
+        ret = min(ret, cal(h + 1, 3) + board[h][2]);
+
+    }
+    else if(c == 3) {
+        ret = min(ret, cal(h + 1, 1) + board[h][3]);
+        ret = min(ret, cal(h + 1, 2) + board[h][3]);
+
+    }
+    
+    dp[h][c] = ret;
+    
+    return ret;
+    
+}
+
+
+int main()
+{
+    scanf("%d", &n);
+    
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= 3; j++) {
+            scanf("%d", &board[i][j]);
+            dp[i][j] = -1;
+        }
+    }
+    printf("%d", min(min(cal(1, 1), cal(1, 2)), cal(1, 3)));
+    
+    
+    
+    return 0;
+}
