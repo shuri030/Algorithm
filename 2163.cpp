3e573e1ccb21f882d2b6473789d79095715eb7d7
shuri030 <kpo01286@gmail.com>
2018-08-02 03:00:00 +0900

    BOJ 2163: 초콜릿 자르기
    BOJ 2163: 초콜릿 자르기

---
+++ 2163.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int split[90010];
+
+int main(void)
+{
+	int n, m;
+
+	scanf("%d %d", &n, &m);
+
+	for(int i = 1; i <= 300; i++) {
+		for(int j = 1; j <= 300; j++) {
+			split[i * j] = (i * j) - 1;
+		}
+	}
+
+	printf("%d", split[n * m]);
+
+	return 0;
+}
