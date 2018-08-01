05d71f32c60f475e1ac7ad0b662b27653b8d25e3
shuri030 <kpo01286@gmail.com>
2018-08-02 02:49:09 +0900

    BOJ 1912: 연속합
    BOJ 1912: 연속합

---
+++ 1912.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int num[100003];
+int dp[100003];
+int ans;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &num[i]);
+    }
+    
+    ans = dp[0] = num[0];
+    
+    for(int i = 1; i < n; i++) {
+        dp[i] = max(dp[i - 1] + num[i], num[i]);
+        if(ans < dp[i]) { ans = dp[i]; }
+    }
+    
+    printf("%d", ans);
+    return 0;
+}
