aa0caf8b8917e5e3927b8ec360450b0f13ccb4ad
shuri030 <kpo01286@gmail.com>
2018-08-02 02:59:14 +0900

    BOJ 2161: 카드 1
    BOJ 2161: 카드 1

---
+++ 2161.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	queue<int> card;
+
+	int N;
+	scanf("%d", &N);
+
+	for(int i = 1; i <= N; i++) {
+		card.push(i);
+	}
+
+	int n;
+
+	while(N > 0) {
+		n = card.front(); 
+		card.pop();
+		printf("%d ", n);
+		N--;
+
+		if(N == 1) { 
+			printf("%d ", card.front()); 
+			break; 
+		}
+		n = card.front();
+		card.pop();
+		card.push(n);
+	}
+
+	return 0;
+}
