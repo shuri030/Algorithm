6e2d2688ebeae27e3f5d402713ece69b2ac780cc
shuri030 <kpo01286@gmail.com>
2018-08-02 03:02:03 +0900

    BOJ 2193: 이친수
    BOJ 2193: 이친수

---
+++ 2193.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <algorithm>
+#include <limits.h>
+#include <queue>
+#include <vector>
+using namespace std;
+
+long long int num[95];
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	num[1] = 1;
+	num[2] = 1;
+	for(int i = 3; i <= 90; i++) {
+		num[i] = num[i - 2] + num[i - 1];
+	}
+
+	printf("%lld", num[n]);
+	
+	return 0;
+}
