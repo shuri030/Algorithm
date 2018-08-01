646e6c28ad3daca19a5c544dcec6bbcb7a572130
shuri030 <kpo01286@gmail.com>
2018-08-02 02:50:35 +0900

    BOJ 1932: 정수 삼각형
    BOJ 1932: 정수 삼각형

---
+++ 1932.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[505][505];
+int tri[505][505];
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            scanf("%d", &tri[i][j]);
+        }
+    }
+    
+    //마지막 줄 초기화
+    for(int i = 1; i <= n; i++) {
+        dp[n][i] = tri[n][i];
+    }
+   
+    for(int i = n - 1; i > 0; i--) {
+        for(int j = 1; j <= i; j++) {
+            dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]) + tri[i][j];
+        }
+    }
+    
+    printf("%d", dp[1][1]);
+    
+}
