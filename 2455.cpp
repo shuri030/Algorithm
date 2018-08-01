eb0ae561df68874eef794dc0abcf22e48ff41037
shuri030 <kpo01286@gmail.com>
2018-08-02 03:06:51 +0900

    BOJ 2455: 지능형 기차
    BOJ 2455: 지능형 기차

---
+++ 2455.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <algorithm>
+#include <limits.h>
+using namespace std;
+
+
+
+
+int main(void)
+{
+	int on = 0, off = 0;
+	int people = 0;
+	int ans = 0;
+
+	for(int i = 1; i <= 4; i++) {
+
+		scanf("%d %d", &on, &off);
+		people = people - on;
+
+		if(people > ans) 
+			ans = people;
+
+		people = people + off;
+
+		if(people > ans) 
+			ans = people;
+
+	}
+
+	printf("%d\n", ans);
+
+	return 0;
+}
