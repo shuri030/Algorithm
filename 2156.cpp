5f2b054beb0f53775b52ae1f8679159935a1f27c
shuri030 <kpo01286@gmail.com>
2018-08-02 02:57:38 +0900

    BOJ 2156: 포도주 시식
    BOJ 2156: 포도주 시식

---
+++ 2156.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <algorithm>
+#include <limits.h>
+using namespace std;
+
+int n;
+int grape[10005];
+int drink[10005][3];
+int ans = 0;
+
+int main(void)
+{
+	scanf("%d", &n);
+
+	for(int i = 1; i <= n; i++) {
+		scanf("%d", &grape[i]);
+	}
+	drink[1][1] = grape[1];
+	drink[2][0] = grape[1];
+	drink[2][1] = grape[2];
+	drink[2][2] = grape[1] + grape[2];
+
+	for(int i = 3; i <= n; i++) {
+		drink[i][0] = max(drink[i - 1][0], max(drink[i - 1][1], drink[i - 1][2]));
+		drink[i][1] = drink[i - 1][0] + grape[i];
+		drink[i][2] = drink[i - 1][1] + grape[i];
+	}
+	
+	ans = max(drink[n][0], max(drink[n][1], drink[n][2]));
+
+	printf("%d", ans);
+	return 0;
+}
