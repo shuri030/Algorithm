b120d9fc4ebbefc9ae65be6467b401d7d6dd161e
shuri030 <kpo01286@gmail.com>
2018-08-02 02:49:55 +0900

    BOJ 1931: 회의실배정
    BOJ 1931: 회의실배정

---
+++ 1931.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <limits.h>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	int N;
+	int s, e;
+	int cnt = 1;
+	int start, end;
+	vector<pair<int, int> > v;
+
+	scanf("%d", &N);
+
+	for(int i = 0; i < N; i++) {
+		scanf("%d %d", &s, &e);
+		v.push_back(make_pair(e, s));
+	}
+
+	sort(v.begin(), v.end());
+	end = v[0].first;
+	start = v[0].second;
+
+	for(int i = 1; i < N; i++) {
+		if(v[i].second >= end) {
+			cnt++;
+			end = v[i].first;
+			start = v[i].second;
+		}
+	}
+
+	printf("%d\n", cnt);
+}
