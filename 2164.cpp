4dc90dd304477e6fe23935247c9212ec692c828c
shuri030 <kpo01286@gmail.com>
2018-08-02 03:00:30 +0900

    BOJ 2164: 카드2
    BOJ 2164: 카드2

---
+++ 2164.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	queue<int> card;
+
+	int N;
+	scanf("%d", &N);
+
+	for(int i = 1; i <= N; i++) {
+		card.push(i);
+	}
+
+	int n;
+
+	while(1) {
+		n = card.front(); 
+		card.pop();
+	
+		if(N == 1) {
+			printf("%d", n); 
+			break; 
+		}
+
+		n = card.front();
+		card.pop();
+		card.push(n);	
+		N--;
+	}
+
+	return 0;
+}
