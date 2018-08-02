fdc05519935261e20c5dc4136ac11c3c11380c45
shuri030 <kpo01286@gmail.com>
2018-08-02 22:51:12 +0900

    선택정렬(selection sort)
    백준 2750 기준

---
+++ 2750_bubble_sort.cpp
@@ -0,0 +1,35 @@
+﻿// < 버블정렬 bubble sort >
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int num[1002];
+
+int main()
+{	
+	int N;
+	scanf("%d", &N);
+	
+	for(int i = 0; i < N; i++) {
+		scanf("%d", &num[i]);
+	}
+
+	int tmp;
+	//옆 값이랑 비교해서 작은 값을 왼쪽으로 보낸다
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N - 1; j++) {
+			if(num[j] > num[j + 1]) {
+				tmp = num[j];
+				num[j] = num[j + 1];
+				num[j + 1] = tmp;
+			}
+		}
+	}
+
+	for(int i = 0; i < N; i++) {
+		printf("%d\n", num[i]);
+	}
+	
+    return 0;
+}
