362828ba93ddf4a8cd7f733b16356b010652a35e
shuri030 <kpo01286@gmail.com>
2018-08-03 20:46:45 +0900

    BOJ 1181: 단어 정렬
    BOJ 1181: 단어 정렬
    sort함수, compare함수 수정

---
+++ 1181.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include<string>
+#include <algorithm>
+using namespace std;
+
+string word[20000];
+int n;
+
+bool compare(string a, string b)
+{
+	if(a.length() < b.length()) { //길이가 짧은 단어부터 우선순위를 준다
+		return 1;
+	}
+	else if(a.length() > b.length()) {
+		return 0;
+	}
+	else { //같으면 사전 순
+		return a < b;
+	}
+}
+
+int main()
+{	
+	int N, num;
+	cin >> N;
+	
+	for(int i = 0; i < N; i++) {
+		cin >> word[i];
+	}
+
+	sort(word, word + N, compare);
+
+	for(int i = 0; i < N; i++) {
+		if(i > 0 && word[i] == word[i - 1]) { continue; } //같으면 출력하지마
+		cout << word[i] << "\n";
+	}
+	
+    return 0;
+}
