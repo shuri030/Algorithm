13aef8e2ad46b95e707a47b92e35c453c9780a9f
shuri030 <kpo01286@gmail.com>
2018-08-03 23:31:51 +0900

    BOJ 10814: 나이순 정렬
    BOJ 10814: 나이순 정렬

---
+++ 10814_struct.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct person {
+	int age;
+	char name[101];
+
+	bool operator <(const person &another) const {
+		return this->age < another.age;
+	}
+};
+person p[100001];
+
+int main()
+{	
+	int N;
+	scanf("%d", &N);
+	
+	//구조체 배열을 쓰는 것이 시간이 덜 걸렸지만 메모리는 많이 차지
+	for(int i = 0; i < N; i++) {
+		scanf("%d %s", &p[i].age, p[i].name);
+	}
+
+	stable_sort(p, p + N);
+	//우선순위가 중복될 경우 순서가 바뀌지 않아야함(안정적)
+	//그냥 sort함수는 우선순위 중복시 전과 같은 순서를 보장하지 않는다.
+
+	for(int i = 0; i < N; i++) {
+		printf("%d %s\n", p[i].age, p[i].name);
+	}
+	
+    return 0;
+}
---
+++ 10814_vector.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<int, string> a, pair<int, string> b)
+{
+	return a.first < b.first;
+}
+int main()
+{	
+	int N;
+	vector<pair<int, string> > v;
+	int age;
+	string name;
+		
+	//pair vector를 쓰니 시간은 오래걸리지만 메모리는 덜 차지	
+	scanf("%d", &N);
+	
+	for(int i = 0; i < N; i++) {
+		cin >> age >> name;
+		v.push_back(pair<int, string>(age, name));
+	}
+
+	stable_sort(v.begin(), v.end(), compare);
+	//우선순위가 중복될 경우 순서가 바뀌지 않아야함(안정적)
+	//그냥 sort함수는 우선순위 중복시 전과 같은 순서를 보장하지 않는다.
+
+	for(int i = 0; i < N; i++) {
+		cout << v[i].first << " " << v[i].second << "\n";
+	}
+	
+    return 0;
+}
