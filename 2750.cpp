6851512961cc640f78082a29df4aa43256417ace
shuri030 <kpo01286@gmail.com>
2018-08-03 22:38:00 +0900

    BOJ 2750: 수 정렬하기1
    BOJ 2750: 수 정렬하기1

---
+++ 2750.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int num[1001];
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	for(int i = 0; i < N; i++) {
+		scanf("%d", &num[i]);
+	}
+
+	sort(num, num + N);
+
+	for(int i = 0; i < N; i++) {
+		printf("%d\n", num[i]);
+	}
+
+	return 0;
+}
