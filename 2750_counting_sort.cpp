b65f8182efcadab2e3ad0b01e7f71656c48cad79
shuri030 <kpo01286@gmail.com>
2018-08-03 19:24:12 +0900

    계수정렬(counting sort)
    계수정렬(counting sort)

---
+++ 2750_counting_sort.cpp
@@ -0,0 +1,27 @@
+﻿// < 계수 정렬 counting sort >
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int c[10000];
+
+int main()
+{	
+	int N, num;
+	scanf("%d", &N);
+	
+	for(int i = 0; i < N; i++) {
+		scanf("%d", &num);
+		c[num - 1]++;
+	}
+
+	for(int i = 0; i < 10000; i++) {
+		if(c[i] != 0) {
+			for(int j = 0; j < c[i]; j++) {
+				printf("%d\n", i + 1);
+			}
+		}
+	}
+	
+    return 0;
+}
