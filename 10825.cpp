dfbdc4ced8924078490e8c1059742eb4683ee66f
shuri030 <kpo01286@gmail.com>
2018-08-04 13:08:35 +0900

    BOJ 10825: 국영수
    BOJ 10825: 국영수

---
+++ 10825.cpp
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct person {
+	string name;
+	int kor;
+	int eng;
+	int math;
+
+	bool operator <(const person &an) const {
+		if(kor != an.kor) {
+			return kor > an.kor;
+		}
+	else {
+		if(eng == an.eng) {
+			if(math == an.math) {
+				return name < an.name;
+			}
+			return math > an.math;
+		}
+		return eng < an.eng;
+	}
+
+	}
+};
+person p[100000];
+
+int main()
+{	
+	int N;
+	scanf("%d", &N);
+	
+	for(int i = 0; i < N; i++) {
+		cin >> p[i].name >> p[i].kor >> p[i].eng >> p[i].math;
+	}
+
+	stable_sort(p, p + N);
+
+	for(int i = 0; i < N; i++) {
+		cout << p[i].name << "\n";
+	}
+	
+    return 0;
+}
