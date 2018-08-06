617d8ec15942fd1204f123d840a08faa5bda70de
shuri030 <kpo01286@gmail.com>
2018-08-07 01:33:47 +0900

    BOJ 1551: 수열의 변화
    BOJ 1551: 수열의 변화

---
+++ 1551.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+
+using namespace std;
+
+int a[21];
+
+int main()
+{	
+	int N, K;
+	scanf("%d %d", &N, &K);
+
+	for(int i = 0; i < N; i++) {
+		scanf("%d", &a[i]);
+		getchar(); // ',' 입력받기
+	}
+	
+	while(K > 0) {
+		for(int i = 0; i < N - 1; i++) {
+			a[i] = a[i + 1] - a[i];
+		}
+		K--; N--;
+	}
+	
+	for(int i = 0; i < N; i++) {
+		printf("%d", a[i]);
+		if(i != N - 1) { printf(","); }
+	}
+    return 0;
+}
