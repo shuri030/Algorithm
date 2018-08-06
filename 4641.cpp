987622b67b1f0503901bb1f41e5d69898a2cb06e
shuri030 <kpo01286@gmail.com>
2018-08-06 23:55:36 +0900

    BOJ 4641: Doubles
    BOJ 4641: Doubles

---
+++ 4641.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> v;
+
+int main()
+{	
+	int n, d, cnt = 0;
+
+	while(1) {
+		while(1) {
+			scanf("%d", &n);
+			if(n == -1) { return 0; }
+			else if(n != 0) {v.push_back(n); }
+			else { break; }
+		}
+
+		for(int i = 0; i < v.size(); i++) {
+			d = v[i] * 2;
+			for(int j = 0; j < v.size(); j++) {
+				if(d == v[j]) {
+					cnt++;
+				}
+			}
+		}
+
+		printf("%d\n", cnt);
+		v.clear();
+		cnt = 0;
+	}
+    return 0;
+}
