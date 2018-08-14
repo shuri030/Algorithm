c50f89b489a9d019af3cd105313fcf6056b27464
Chohanseul <kpo01286@gmail.com>
2018-08-14 16:30:58 +0900

    BOJ 10974: 모든 순열

---
+++ Backtracking/10974.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+
+using namespace std;
+
+int num[9]; //만든 수열 저장
+int visit[9]; //방문 처리 배열
+int n;
+
+void go(int k)
+{
+    if(k > n) {
+        for(int i = 1; i <= n; i++) {
+            printf("%d ", num[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+
+    for(int i = 1; i <= n; i++) {
+        if(visit[i] == 0) {
+            num[k] = i;
+            visit[i] = 1;
+            go(k + 1);
+            visit[i] = 0;
+        }
+
+    }
+
+    return;
+}
+int main()
+{
+
+    scanf("%d", &n);
+
+    go(1);
+    return 0;
+}
